da87459c34d865d69803cf1431a96d3314c7a4c6
Andrew Grishin <kirivanov777@gmail.com>
2023-02-06 02:46:25 +0300

    start to add physics

---
+++ src/kikan/physics.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from kikan.entity import Entity
+
+
+@dataclass
+class PhysicsWorldConfig:
+    FREE_FALL_FORCE: float = 9.81
+
+
+class PhysicsEnvironment:
+    def __init__(self, entity):
+        self.config = PhysicsWorldConfig()
+
+    def tick(self, entity: Entity):
+        entity.pos += entity.velocity
